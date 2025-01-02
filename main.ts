a49293c5ce2d2f47bef95f56a57406b9394deb78
SnO2WMaN <me@sno2wman.net>
2025-01-02 15:03:36 +0900

    more detailed info

--- main.ts
+++ main.ts
@@ -1,67 +1,136 @@
-import {
-  DOMParser,
-  Element,
-} from "https://deno.land/x/deno_dom@v0.1.43/deno-dom-wasm.ts";
+import { DOMParser } from "jsr:@b-fuze/deno-dom";
 
-const pageUrls = [
-  "http://oto10.s602.xrea.com/10sen/2008-r.html",
-  "http://oto10.s602.xrea.com/10sen/2009-r.html",
-  "http://oto10.s602.xrea.com/10sen/2010-r.html",
-  "http://oto10.s602.xrea.com/10sen/2011-r.html",
-  "http://oto10.s602.xrea.com/10sen/2012-r.html",
-  "http://oto10.s602.xrea.com/10sen/2013-r.html",
-  "http://oto10.s602.xrea.com/10sen/2014-r.html",
-  "http://oto10.s602.xrea.com/10sen/2015-r.html",
-  "http://oto10.s602.xrea.com/10sen/2016-r.html",
-  "http://oto10.s602.xrea.com/10sen/2017-r.html",
-  "http://oto10.s602.xrea.com/10sen/2018-r.html",
-  "http://oto10.s602.xrea.com/10sen/2019-r.html",
-  "http://oto10.s602.xrea.com/10sen/2020-r.html",
-  "http://oto10.s602.xrea.com/10sen/2021-r.html",
-  "http://oto10.s602.xrea.com/10sen/2022-r.html",
-  "http://oto10.s602.xrea.com/10sen/2023-r.html",
-  "http://oto10.s602.xrea.com/10sen/2024-r.html",
+const years = [
+  2008,
+  2009,
+  2010,
+  2011,
+  2012,
+  2013,
+  2014,
+  2015,
+  2016,
+  2017,
+  2018,
+  2019,
+  2020,
+  2021,
+  2022,
+  2023,
+  2024,
 ];
 
-const vurls = await Promise.all(
-  pageUrls.map(async (url) => {
-    const b = await fetch(url);
-    const t = await b.text();
-    const doc = new DOMParser().parseFromString(t, "text/html")!;
-    const qq = doc.querySelectorAll("#content_block_1 a");
+const dat: Record<
+  number,
+  {
+    count: number;
+    url: string | null;
+    title: string;
+    type: ReturnType<typeof estimateType>;
+  }[]
+> = {};
 
-    return [...qq]
-      .map((e) => {
-        const vurl = (e as Element).attributes
-          .getNamedItem("href")!
-          .value.trim();
+for (const year of years) {
+  const url = `http://oto10.s602.xrea.com/10sen/${year}-r.html`;
+  const t = await fetch(url).then((b) => b.text());
+  const a = new DOMParser().parseFromString(t, "text/html")!;
+  const dds = a.querySelectorAll("tr");
 
-        // 2011年『膳(Big_blue) 』
-        if (vurl === "http://www.nicovideo.jp/tag/Big_blue")
-          return "https://www.nicovideo.jp/watch/sm13274282";
+  const recs: (typeof dat)[number] = [];
 
-        // 2010年『ヤマザキ春のTimepiece pan まIIり』
-        if (vurl === "http://www.nicovideo.jp/watch/1270563585")
-          return "https://www.nicovideo.jp/watch/sm27116924";
+  for (const dd of dds) {
+    const tds = dd.querySelectorAll("td");
 
-        // 2010年『アレミミズ』（詳細不明）
-        if (vurl === "http://www.nicovideo.jp/watch/1286180411") return null;
+    const z0 = tds[0].textContent.trim();
 
-        if (/^sm\d+$/.test(vurl))
-          return `https://www.nicovideo.jp/watch/${vurl}`;
+    if (!/^\d+$/.test(z0)) continue;
 
-        return vurl;
-      })
-      .filter((e): e is string => e !== null)
-      .map((u) => {
-        const p = new URL(u);
-        if (p.protocol === "http:") p.protocol = "https:";
-        return p.toString();
-      });
-  })
-).then((t) => t.reduce((a, b) => [...b, ...a], [] as string[]).toSorted());
+    const href =
+      (tds[1].querySelector("a")?.attributes.getNamedItem("href")?.value
+        .trim()) ||
+      null;
+    const vurl = normalizeVUrl(href);
+
+    recs.push({
+      count: parseInt(z0, 10),
+      url: vurl,
+      title: tds[2].textContent.trim(),
+      type: estimateType(vurl),
+    });
+  }
+
+  dat[year] = recs;
+}
+
+function normalizeVUrl(vurl: string | null): string | null {
+  if (vurl === null) return null;
+
+  // 2011年『膳(Big_blue) 』
+  if (vurl === "http://www.nicovideo.jp/tag/Big_blue") {
+    return "https://www.nicovideo.jp/watch/sm13274282";
+  }
+
+  // 2010年『ヤマザキ春のTimepiece pan まIIり』
+  if (vurl === "http://www.nicovideo.jp/watch/1270563585") {
+    return "https://www.nicovideo.jp/watch/sm27116924";
+  }
+
+  // 2010年『アレミミズ』（詳細不明）
+  if (vurl === "http://www.nicovideo.jp/watch/1286180411") return null;
+
+  if (/^sm\d+$/.test(vurl)) {
+    return `https://www.nicovideo.jp/watch/${vurl}`;
+  }
+
+  return vurl;
+}
+
+function estimateType(
+  vurl: string | null,
+):
+  | "niconico"
+  | "nicovideo-live"
+  | "youtube"
+  | "twitter"
+  | "bilibili"
+  | "soundcloud"
+  | "linevoom"
+  | "unknown" {
+  if (vurl === null) return "unknown";
+
+  switch (new URL(vurl).host) {
+    case "www.nicovideo.jp":
+      return "niconico";
+
+    case "live.nicovideo.jp":
+      return "nicovideo-live";
+
+    case "www.youtube.com":
+    case "m.youtube.com":
+    case "youtu.be":
+      return "youtube";
+
+    case "twitter.com":
+    case "www.twitter.com":
+    case "x.com":
+      return "twitter";
+
+    case "bilibili.com":
+    case "www.bilibili.com":
+      return "bilibili";
+
+    case "soundcloud.com":
+      return "soundcloud";
+
+    case "linevoom.line.me":
+      return "linevoom";
+
+    default:
+      return "unknown";
+  }
+}
 
 await Deno.writeFile(
   "pages/data.json",
-  new TextEncoder().encode(JSON.stringify(vurls))
+  new TextEncoder().encode(JSON.stringify(dat)),
 );
